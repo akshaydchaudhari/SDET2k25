5ab47b950150fedd606b50d3b9c0e8593c7e448c
akshay <akshaydchaudhari1@gmail.com>
2025-02-04 16:41:31 +0530

    code

--- src/main/java/JavaCoding25/P20CharacterOccurrenceInString.java
+++ src/main/java/JavaCoding25/P20CharacterOccurrenceInString.java
@@ -37,4 +37,26 @@ public class P20CharacterOccurrenceInString {
             System.out.println("Character :" + entry.getKey() + " - Occurrence: " + entry.getValue());
         }
     }
+
+    public static void countCharacterOccurrence1(String str) {
+
+        Map<Character, Integer> charCountMap = new HashMap<>();
+
+        // Convert the string to a character array
+        char[] strArray = str.toCharArray();
+
+        // Iterate through the character array
+        for (char c : strArray) {
+
+            // If the character is not a space, count its occurrence
+            if (c != ' ') {
+                if (charCountMap.containsKey(c)) {
+                    // If the character is already in the map, increment its count
+                    charCountMap.put(c, charCountMap.get(c) + 1);
+                } else {
+                    // If the character is not in the map, add it with a count of 1
+                    charCountMap.put(c, 1);
+                }
+            }
+        }
 }
