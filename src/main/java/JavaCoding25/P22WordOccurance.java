d90696e61f34e5b0d80acc164d02827fbfcdb540
akshay <akshaydchaudhari1@gmail.com>
2025-02-15 17:25:59 +0530

    occurrance of word

---
+++ src/main/java/JavaCoding25/P22WordOccurance.java
@@ -0,0 +1,36 @@
+package JavaCoding25;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P22WordOccurance {
+
+    public static void main(String[] args) {
+
+        String str = "Akshay is the best Akshay is perfect";
+
+        String[] str1 = str.split(" ");
+
+        findWordOccurance(str1);
+    }
+
+    private static void findWordOccurance(String[] str1) {
+
+        Map<String, Integer> freqOfWord = new HashMap<>();
+
+        for (String word : str1) {
+
+            freqOfWord.put(word, freqOfWord.getOrDefault(word, 0) + 1);
+        }
+
+        System.out.println(freqOfWord);   // {the=1, Akshay=2, perfect=1, is=2, best=1}
+
+        for (Map.Entry<String, Integer> entry : freqOfWord.entrySet()) {
+
+            System.out.println("Word:" + entry.getKey() + " : " + entry.getValue());
+
+
+        }
+    }
+
+}
